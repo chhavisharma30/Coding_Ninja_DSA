3cb872cad612f8db5dfe4614600ce61bca2eb143
Chhavi <chhavisharma3007@gmail.com>
2022-11-21 21:40:25 +0530

    Added inbuiltUnorderedMap code

---
+++ hashmaps/inbuiltUnorderedMap.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<unordered_map>
+#include<string>
+using namespace std;
+
+int main(){
+    unordered_map<string,int> myMap;
+
+    //insert
+    pair<string,int> p("one",1);
+    myMap.insert(p);
+
+    myMap["two"]=2;
+
+    //search
+    cout<<myMap.at("one")<<endl;
+    cout<<myMap["two"]<<endl;
+
+    //size
+    cout<<myMap.size()<<endl;
+
+    //searching a key which does not exist
+    cout<<myMap["three"]<<endl;     
+    //cout<<myMap.at("three")<<endl;    gives exception
+
+    cout<<myMap.size()<<endl;
+
+    //check presence
+    if(myMap.count("three")>0)
+        cout<<"three is present!"<<endl;
+
+    //erase
+    myMap.erase("three");
+
+    cout<<myMap.size()<<endl;
+}
